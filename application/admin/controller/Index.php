6368874b84587077872b953c80bc7aab7a10668a
vipnol <376377656@qq.com>
2018-07-11 11:59:25 +0800

    第一次提交

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+    	return view();
+    }
+
+    public function console()
+    {
+    	return view();
+    }
+
+    public function homepage1()
+    {
+    	return view();
+    }
+
+    public function homepage2()
+    {
+    	return view();
+    }
+
+    public function login()
+    {
+    	return view();
+    }
+
+
+
+}
